09b1c4cb9863fd74abf934ed31d8cd484d1e4f4a
smejias <santiagojmejias@gmail.com>
2025-11-12 18:04:19 -0300

    ESC for pause

--- Assets/Components/States/DamagedState.cs
+++ Assets/Components/States/DamagedState.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class DamagedState : State
 {
-    [SerializeField] private float recoveryTime = 1f;
+    [SerializeField] private float recoveryTime = 3f;
     private float timer;
 
     public override void OnEnter()
@@ -15,7 +15,7 @@ public class DamagedState : State
     {
         timer += Time.deltaTime;
         if (timer >= recoveryTime)
-            stateMachine.ChangeState<PatrolState>();
+            stateMachine.ChangeState<AlertState>();
     }
 
     public override void OnExit()
